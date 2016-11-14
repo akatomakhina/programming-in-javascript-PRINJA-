3fe07ea97cde4d27a21c5ff4cf933007d44c5cfc
akatomakhina <katomakhinaanastasiya@gmail.com>
2016-11-14 17:22:32 +0300

    Files has been updated

--- js/script.js
+++ js/script.js
@@ -6,15 +6,22 @@ var func = function() {
   $.ajax({
     type: 'GET',
       //url:'http://localhost:597',
-      url: 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=42d91bc90a5350bbe2bfef8cecab14f5&format=json&limit=10',
+      url: 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=42d91bc90a5350bbe2bfef8cecab14f5&format=json&limit=9',
       success:function(data) { 
         console.log(data); 
           var template = Handlebars.compile( $('#template').html());
-          $('.updates').append(template(data));
+          $('.updates').empty().append(template(data));
         },
-        error:function(data) {
-          console.log("error"); 
-          console.log(data); 
+        error:function(XMLHttpRequest, textStatus, errorThrown) {
+          if (XMLHttpRequest.readyState == 4) {
+              alert("Http error");
+        }
+        else if (XMLHttpRequest.readyState == 0) {
+            alert("network error");
+        }
+        else {
+            alert("wtf");
+        } 
         }
   });
 }

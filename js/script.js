3354705a603240801d3306ef58a84b433261028e
akatomakhina <katomakhinaanastasiya@gmail.com>
2016-12-09 21:39:55 +0300

    New file added

--- js/script.js
+++ js/script.js
@@ -3,28 +3,39 @@
 var token = '42d91bc90a5350bbe2bfef8cecab14f5';
 
 var func = function() {
+
+  var errorBHttp = document.getElementById('error-http');
+  var errorBNetwork = document.getElementById('error-network');
   $.ajax({
     type: 'GET',
+    timeout: 5000,
       //url:'http://localhost:597',
       url: 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=42d91bc90a5350bbe2bfef8cecab14f5&format=json&limit=9',
-      success:function(data) { 
-        console.log(data); 
+      success:function(data) {
+        console.log(data);
           var template = Handlebars.compile( $('#template').html());
           $('.updates').empty().append(template(data));
         },
+
+
         error:function(XMLHttpRequest, textStatus, errorThrown) {
           if (XMLHttpRequest.readyState == 4) {
-              alert("Http error");
+              console.log("HTTP ERROR");
+              $('#tracks').attr('visibility', 'hidden');
+              errorBHttp.style.display = "block";
         }
         else if (XMLHttpRequest.readyState == 0) {
-            alert("network error");
+            console.log("NETWORK ERROR");
+            $('#tracks').attr('visibility', 'hidden');
+            errorBNetwork.style.display = "block";
         }
         else {
-            alert("wtf");
-        } 
+            alert("UNKNOWN ERROR ");
+        }
         }
   });
 }
+
 
  $(document).ready(function() {
   func();
